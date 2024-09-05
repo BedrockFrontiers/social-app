d219167372a62cf80f278e00cca94ac709cf4521
Ueslei Paim <uesleibros@gmail.com>
2024-09-04 23:14:05 -0300

    Update index.jsx

--- src/components/LeftSideBar/index.jsx
+++ src/components/LeftSideBar/index.jsx
@@ -15,19 +15,17 @@ export default function LeftSideBar() {
   return (
     <aside className="w-[250px] h-[100vh] overflow-y-auto p-4 bg-white text-black border-r border-gray-300">
       <div className="pt-4 flex flex-col gap-6">
-        {/* Social App Logo */}
         <div className="flex justify-center mb-8">
-          <Image src="/icon.png" width={40} height={40} alt="Social App Logo" />
+          <Image className="select-none" src="/icon.png" width={40} height={40} alt="Social App Logo" />
         </div>
 
-        {/* Navigation Items */}
         <div className="flex flex-col gap-2">
           <SidebarItem src="/icons/home.svg" text="Home" />
           <SidebarItem src="/icons/notifications.svg" text="Notifications" />
           <SidebarItem src="/icons/profile.svg" text="Profile" />
+          <SidebarItem src="/icons/settings.svg" text="Settings" />
         </div>
 
-        {/* Tweet Button */}
         <div className="mt-auto">
           <button className="p-3 w-full rounded-full font-bold bg-blue-500 text-white transition duration-200 hover:opacity-90">
             New Tide
