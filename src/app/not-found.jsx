31a109299dd72eba322a59b9b655789ef408f9a0
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-09-05 18:31:26 -0300

    fix: `'` can be escaped with `&apos;`

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,7 +3,7 @@ import MainStructure from "@/components/MainStructure";
 export default function NotFound() {
 	return (
 		<MainStructure className="p-4">
-			<h2 className="text-xl font-bold mx-auto">Are you lost? Unfortunately, This page doesn't exist.</h2>
+			<h2 className="text-xl font-bold mx-auto">Are you lost? Unfortunately, This page doesn&apos;t exist.</h2>
 		</MainStructure>
 	);
 }
