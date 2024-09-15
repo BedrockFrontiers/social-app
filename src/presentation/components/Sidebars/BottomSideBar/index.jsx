49aa97a457b8af9ed372ae66fcb82343a454d2f6
Ueslei Paim <uesleibros@gmail.com>
2024-09-15 00:52:28 -0300

    Update index.jsx

--- src/presentation/components/Sidebars/BottomSideBar/index.jsx
+++ src/presentation/components/Sidebars/BottomSideBar/index.jsx
@@ -5,7 +5,7 @@ export default function BottomSideBar({ user }) {
 	const isLogged = Object.keys(user || {}).length > 0;
 
 	return (
-		<div className="fixed z-50 bottom-0 left-0 w-full bg-white dark:bg-black border-t dark:border-zinc-700 py-3 min-h-[50px]">
+		<div className="fixed z-10 bottom-0 left-0 w-full bg-white dark:bg-black border-t dark:border-zinc-700 py-3 min-h-[50px]">
 			<div className="flex gap-4 justify-center h-full overflow-x-auto items-center">
 				{(isLogged) ? <LoggedItems /> : <UnloggedItems />}
 			</div>
