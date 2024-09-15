d494edbc48b39673ffc4e352d5bab5dc9ceb412d
Ueslei Paim <uesleibros@gmail.com>
2024-09-14 22:08:49 -0300

    Update not-found.jsx

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
 	return (
 		<MainStructure className="p-4">
 			<h2 className="text-xl font-bold mx-auto">Are you lost? Unfortunately, This page doesn&apos;t exist.</h2>
-			<Image src="https://http.cat/404" width={700} height={700} className="mt-4 select-none" />
+			<Image src="https://http.cat/404" width={700} height={700} className="mt-4 select-none" alt="Not Found" />
 		</MainStructure>
 	);
 }
