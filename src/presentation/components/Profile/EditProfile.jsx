379f587cfcfcfdd6535b9ec262425e4f8b745008
Ueslei Paim <uesleibros@gmail.com>
2024-09-15 00:18:35 -0300

    Update EditProfile.jsx

--- src/presentation/components/Profile/EditProfile.jsx
+++ src/presentation/components/Profile/EditProfile.jsx
@@ -65,7 +65,7 @@ export default function EditProfile({ user, onClose }) {
 
 	return (
 		<div className="fixed top-0 left-0 z-50 bg-black bg-opacity-70 w-full h-full flex items-center justify-center p-4">
-			<div className="lg:rounded-xl bg-white dark:bg-zinc-900 themed-border p-4 lg:w-[550px] max-lg:w-full lg:max-h-[500px] max-lg:h-full max-lg:max-h-full overflow-y-auto">
+			<div className="relative lg:rounded-xl bg-white dark:bg-zinc-900 themed-border p-4 max-lg:w-full lg:w-[550px] max-lg:h-full lg:max-h-[500px] max-lg:max-h-full overflow-y-auto">
 				<div>
 					<h3 className="text-xl font-semibold">Edit Profile</h3>
 					<hr className="themed-border" />
