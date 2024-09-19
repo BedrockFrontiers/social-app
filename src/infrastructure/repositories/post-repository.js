9b36f815b880a3405338f4356a4c7282372f4bde
Ueslei Paim <uesleibros@gmail.com>
2024-09-18 23:03:00 -0300

    Create post-repository.js

---
+++ src/infrastructure/repositories/post-repository.js
@@ -0,0 +1,55 @@
+import prisma from "@/db.js";
+import Post from "@/domain/entities/post/post";
+
+export default class PostRepository {
+  async create(post) {
+    const newPost = await prisma.post.create({
+      data: {
+        gid: post.gid,
+        content: post.content,
+        attachments: post.attachments,
+        createdAt: new Date(),
+      },
+      include: {
+        author: true,
+      }
+    });
+
+    return new Post(newPost);
+  }
+  
+  async findById(postId) {
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: {
+        author: true, 
+        likes: true, 
+        reposts: true, 
+      },
+    });
+
+    return post ? new Post(post).toJSON() : null;
+  }
+
+  async delete(postId) {
+    await prisma.post.delete({
+      where: { id: postId },
+    });
+  }
+
+  async findAll() {
+    const posts = await prisma.post.findMany({
+      include: {
+        author: true, 
+        likes: true, 
+        reposts: true, 
+        comments: true, 
+      },
+      orderBy: {
+        createdAt: "desc", 
+      },
+    });
+
+    return posts.map(post => new Post(post).toJSON());
+  }
+}
