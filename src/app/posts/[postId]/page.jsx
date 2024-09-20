dee6c463279496781546c63dd5ebb038b371e571
Ueslei Paim <uesleibros@gmail.com>
2024-09-19 21:49:27 -0300

    Update page.jsx

--- src/app/posts/[postId]/page.jsx
+++ src/app/posts/[postId]/page.jsx
@@ -63,7 +63,7 @@ export default async function ViewPost({ params }) {
 	return (
 		<MainStructure>
 			<MainHeader returnRoute={`/profile/${post.author.identifier}`}>
-				<h3 className="select-none font-bold text-xl max-w-[130px] truncate">{post.author.identifier}</h3> post
+				<h3 className="select-none font-bold text-xl">{post.author.identifier} Post</h3>
 			</MainHeader>
 			<div>
 				<Post post={post} me={me} isLiked={isLiked} linkable={false} />
