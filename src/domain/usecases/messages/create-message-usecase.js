bdf3faf552e744a8594f7fbf0877601140ba99f3
Ueslei Paim <uesleibros@gmail.com>
2024-09-23 06:26:55 -0300

    Update create-message-usecase.js

--- src/domain/usecases/messages/create-message-usecase.js
+++ src/domain/usecases/messages/create-message-usecase.js
@@ -1,3 +1,18 @@
 export default class CreateMessageUseCase {
-	
+	constructor(messageRepository, userRepository) {
+		this.messageRepository = messageRepository;
+		this.userRepository = userRepository;
+	}
+
+	async execute({ gid, content, attachments, recipientId }) {
+		const existingUser = await this.userRepository.findBy(gid);
+    if (!existingUser)
+      throw new Error("G-ID invalid.");
+
+    const existingRecipient = await this.userRepository.findById(recipientId);
+    if (!recipientId)
+      throw new Error("User to send message doesn't exist.");
+
+		await this.messageRepository.create({ content: content, attachments: attachments, senderId: existingUser.id, recipientId: recipientId });
+	}
 }
