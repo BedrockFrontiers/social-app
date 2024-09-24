24713994be9cf4b7c881662e646e4fe1a318204c
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-09-24 18:49:54 -0300

    update: new chat messages ui

--- src/domain/usecases/messages/create-message-usecase.js
+++ src/domain/usecases/messages/create-message-usecase.js
@@ -18,8 +18,4 @@ export default class CreateMessageUseCase {
 
 		await this.messageRepository.create({ content: content, attachments: attachments, senderId: existingUser.id, recipientId: recipientId });
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 66f6192649d6e52d00f4cf39fa7a5f26d72c5c4c
