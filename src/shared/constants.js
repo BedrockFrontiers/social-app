bf5270bde2754f1dbb88f7dd25fe5d74d2b0cbbf
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2024-09-24 18:51:11 -0300

    update: new default avatar url.

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,3 +1,3 @@
-export const DEFAULT_AVATAR_URL = "https://avatar.iran.liara.run/public/48";
+export const DEFAULT_AVATAR_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt0paMtjn_F9f7Z5gqQ5Xb-hkQxMsGBbriDA&usqp=CAU";
 export const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
 export const IMGUR_API_URL = "https://api.imgur.com/3/image";
